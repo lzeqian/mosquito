b81dc74959edcb819bde30f5bd0f5f0c7bb6cd7c
liaomin@gvt861.com <liaomin@gvt861.com>
2020-11-05 15:17:34 +0800

    图片导出支持各种样式类型

--- front/gpm-frontend/src/assets/script/runtime/exports.js
+++ front/gpm-frontend/src/assets/script/runtime/exports.js
@@ -77,7 +77,28 @@ define(function (require, exports, module) {
     }
 
     function exportImage (){
-      png.exportPNGImage(minder);
+      //png.exportPNGImage(minder);
+      minder.exportData("png").then((imgUrl)=>{
+        // 这里是获取到的图片base64编码,这里只是个例子哈，要自行编码图片替换这里才能测试看到效果
+       // 如果浏览器支持msSaveOrOpenBlob方法（也就是使用IE浏览器的时候），那么调用该方法去下载图片
+        if (window.navigator.msSaveOrOpenBlob) {
+          var bstr = atob(imgUrl.split(',')[1])
+          var n = bstr.length
+          var u8arr = new Uint8Array(n)
+          while (n--) {
+            u8arr[n] = bstr.charCodeAt(n)
+          }
+          var blob = new Blob([u8arr])
+          window.navigator.msSaveOrOpenBlob(blob, 'chart-download' + '.' + 'png')
+        } else {
+          // 这里就按照chrome等新版浏览器来处理
+          const a = document.createElement('a')
+          a.href = imgUrl
+          a.setAttribute('download', 'chart-download')
+          a.click()
+        }
+      })
+
     }
 
     function exportSVG (){
